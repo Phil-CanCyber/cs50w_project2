89c09d016f0d5ab699411901210bc625c34a6aee
Phil - CanCyber <62562731+Phil-CanCyber@users.noreply.github.com>
2020-05-05 22:23:31 -0400

    Add files via upload

---
+++ project2-a/static/login.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // By default, submit button is disabled
+  document.querySelector("#submit").disabled = true;
+
+  // Enable button only if there is text in the input field
+  document.querySelector("#username").onkeyup = () => {
+    if (document.querySelector("#username").value.length > 0)
+      document.querySelector("#submit").disabled = false;
+    else document.querySelector("#submit").disabled = true;
+  };
+
+});
